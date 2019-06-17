c5a88a31f0c13da53f40d647aab7524e33fdf61a
muhammad furqonuddin <m.furqonuddin@gmail.com>
2019-06-17 15:50:45 +0700

    soal 3

---
+++ 2.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas.plotting import register_matplotlib_converters
+
+register_matplotlib_converters()
+
+df1 = pd.read_csv('EXCL.JK.csv', index_col= False, parse_dates=['Date'])
+df2 = pd.read_csv('FREN.JK.csv', index_col= False, parse_dates=['Date'])
+df3 = pd.read_csv('ISAT.JK.csv', index_col= False, parse_dates=['Date'])
+df4 = pd.read_csv('TLKM.JK.csv', index_col= False, parse_dates=['Date'])
+
+df11 = pd.DataFrame(df1).set_index('Date')
+df22 = pd.DataFrame(df2).set_index('Date')
+df33 = pd.DataFrame(df3).set_index('Date')
+df44 = pd.DataFrame(df4)
+
+
+perusahaan = np.array(['PT. XL Axiata Tbk', 'PT Smartfren Telecom Tbk', 'PT Indosat Tbk', 'PT Telekomunikasi Indonesia Tbk'])
+# df2 = df2.set_index('id')
+
+
+tgl = df11['Date'].values
+# pd.date_range('201904')
+
+xl = df11['Close'].values
+smart = df22['Close'].values
+ind = df33['Close'].values
+tele = df44['Close'].values
+
+plt.plot(tgl,xl, 'r-')  
+plt.plot(tgl,smart, 'g-')
+plt.plot(tgl,ind, 'b-')
+plt.plot(tgl,tele, 'y-')
+plt.title('Harga Historis Saham Provider Indonesia')
+plt.xlabel('Tanggal')
+plt.ylabel('Rupiah(IDR)')
+plt.xticks(rotation = 60)
+plt.legend(
+    perusahaan,
+    scatterpoints=1,
+    loc='lower left',
+    ncol=3,
+    fontsize=8
+)
+
+plt.tight_layout()
+plt.show()
+
